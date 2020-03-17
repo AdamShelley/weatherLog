a73bcbd6e76e96f25b9f327fae9af375acef7032
Adam Shelley <adam.shelley18@gmail.com>
2020-03-17 21:37:33 +0000

    Updates

---
+++ precache-manifest.18232c2e2e633d6778e83b11144dba53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4576f33ef380f1ce142bae602683cb77",
+    "url": "/weatherLog/index.html"
+  },
+  {
+    "revision": "157665ba37ea5d41104e",
+    "url": "/weatherLog/static/css/main.e37d8758.chunk.css"
+  },
+  {
+    "revision": "34805727779c54cd791f",
+    "url": "/weatherLog/static/js/2.4c2b71ea.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/weatherLog/static/js/2.4c2b71ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157665ba37ea5d41104e",
+    "url": "/weatherLog/static/js/main.c0fcca6d.chunk.js"
+  },
+  {
+    "revision": "5ab628f0c67493a6a6d7",
+    "url": "/weatherLog/static/js/runtime-main.f7f016d8.js"
+  }
+]);
